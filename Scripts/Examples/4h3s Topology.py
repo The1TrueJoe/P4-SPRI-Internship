46ae965491f97c33b20ece48469a83a85a174181
The1TrueJoe <jttelaak@hotmail.com>
2021-06-16 11:32:41 -0400

    Fix error

--- Scripts/Examples/4h3s Topology.py
+++ Scripts/Examples/4h3s Topology.py
@@ -2,7 +2,7 @@ from mininet.net import Mininet
 from mininet.node import Controller, RemoteController, OVSController
 from mininet.node import CPULimitedHost, Host, Node
 from mininet.node import OVSKernelSwitch, UserSwitch
-from mininet.nde import IVSSwitch
+from mininet.node import IVSSwitch
 from mininet.cli import CLI
 from mininet.log import setLogLevel, info
 from mininet.link import TCLink, Intf
